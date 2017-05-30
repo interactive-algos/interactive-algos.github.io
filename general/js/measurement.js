18673d72b7ad1dfcc37f8d84e7fe7ece8766b675
Kelvin Zhang <zhangxp1998@gmail.com>
2017-05-30 00:03:54 -0400

    Set the API for Measurement Model

--- general/js/measurement.js
+++ general/js/measurement.js
@@ -1,15 +1,5 @@
-function Measurement(lastState, state)
-{
-	this.lastX = lastState.x;
-	this.lastY = lastState.y;
-	this.lastDir = lastState.dir;
-	
-	this.x = state.x;
-	this.y = state.y;
-	this.dir = state.dir;
-}
 
-function OdometryModel(a1, a2, a3, a4)
+function BeamModel(a1, a2, a3, a4)
 {
 	//Noise on rotational 1
 	this.a1 = a1;
@@ -21,43 +11,26 @@ function OdometryModel(a1, a2, a3, a4)
 	this.a4 = a4;
 }
 
-//Sample from the distribution p(x_t+1, u, x), 
-//where u is the control/odometry, x is the state
-OdometryModel.prototype.sample = function(u, state)
+/*
+	Probability of getting measurement z
+	if the robot pose is 'state'. (Given map m)
+ */
+BeamModel.prototype.probability = function(z, state, m)
 {
-	var dx = u.x-u.lastX;
-	var dy = u.y-u.lastY;
+	//Particle/Robot's state
+	console.log("Robot is at (" + state.x, + ", " + state.y + ") direction: " + state.dir);
 
-	var a1 = this.a1;
-	var a2 = this.a2;
-	var a3 = this.a3;
-	var a4 = this.a4;
+	//z will be an array of distanced, representing
+	for(var i = 0; i < z.length; i ++)
+	{
+		console.log("Ray at angle " + i/z.length * Math.PI*2 + "detected distance: " + z[i]);
+	}
 
-	//first rotation
-	var rot1 = atan2(dy, dx) - u.lastDir;
-	//translational distance traveled
-	var trans = sqrt(dx*dx + dy*dy);
-	//second rotation
-	var rot2 = u.dir - u.lastDir - rot1;
-
-	//Bound radians to interval [-pi, pi]
-	rot1 = boundRadian(rot1);
-	rot2 = boundRadian(rot2);
-
-	var rot1_squared = rot1*rot1;
-	var trans_squared = trans*trans;
-	var rot2_squared = rot2*rot2;
-
-	//actual sampling
-	rot1 += gaussian() * sqrt(a1*rot1_squared + a2*trans_squared);
-	trans += gaussian() * sqrt(a3*trans_squared + a4*rot1_squared + a4*rot2_squared);
-	rot2 += gaussian() * sqrt(a1*rot2_squared + a2*trans_squared);
-
-	var x = state.x + trans*cos(state.dir+rot1);
-	var y = state.y + trans*sin(state.dir+rot1);
-
-	//disregarding final rotation for now...
-	var dir = state.dir + rot1 + rot2;
-
-	return new RobotState(x, y, dir);
+	//m is the map
+	for(var i = 0; i < m.length; i ++)
+	{
+		var line = m[i];
+		console.log('Line Segment from' + l.s + " to " + l.t + " slope: " + (l.t.y-l.s.y)/(l.t.x-l.s.x));
+	}
+	return random();
 };
