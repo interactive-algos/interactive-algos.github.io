9e06065d68107672930297a03e450f1e5c6d0b4a
Kelvin Zhang <zhangxp1998@gmail.com>
2017-06-07 08:54:29 -0400

    fix missing line on on map

--- general/js/map_data.js
+++ general/js/map_data.js
@@ -6,6 +6,7 @@ function getMapForCanvas(canvas)
         new Line(0, 0, canvas.width, 0),
         new Line(canvas.width, 0, canvas.width, canvas.height),
         new Line(canvas.width, canvas.height, 0, canvas.height),
+		new Line(0, canvas.height, 0, 0),
         new Line(30, 0, 110, 130),
         new Line(110, 130, 220, 60),
         new Line(270, 0, 370, 130),
@@ -20,13 +21,6 @@ function getMapForCanvas(canvas)
         new Line(220, 270, 220, 400),
         new Line(220, 270, 290, 270),
         new Line(370, 270, 450, 270)
-        // new Line(0, canvas.height, 0, 0),
-        // new Line(0, 100, 300, 100),
-        // new Line(300, 70, 300, 0),
-        // new Line(0, 250, 400, 250),
-        // new Line(450, 250, canvas.width, 250),
-        // new Line(500, 0, 500, 100),
-        // new Line(500, 100, canvas.width, 100)
     ];
     return map;
 }
