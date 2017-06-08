ead24cd78bffaa06bbe7c5c007d69d3a344f2876
Kelvin Zhang <zhangxp1998@gmail.com>
2017-06-08 18:50:48 -0400

    format

--- general/js/particle_filter.js
+++ general/js/particle_filter.js
@@ -1,4 +1,3 @@
-
 /**
  * A simulation of Particle Filter --
  * A representation of nonparametric implementation of the Bayes filter
@@ -8,27 +7,32 @@
  * @param {BeamModel} sensorModel - The sensor model that is used to measure environment over time.
  * @param {RobotState} robotState - The state of the robot while creating the particle filter
  */
-function ParticleFilter(particleCount, motionModel, sensorModel, robotState){
-    this.count = particleCount;
-    // Init the array that stores particles
-    this.particles = new Array(particleCount);
+function ParticleFilter(particleCount, motionModel, sensorModel, robotState)
+{
+	this.count = particleCount;
+	// Init the array that stores particles
+	this.particles = new Array(particleCount);
 
-    this.motionModel = motionModel;
-    this.sensorModel = sensorModel;
+	this.motionModel = motionModel;
+	this.sensorModel = sensorModel;
 
-    if(typeof robotState !== 'undefined'){
-        // If current robot state is already defined
-        // generate initial particles around the robot
-        for (var i = particleCount - 1; i >= 0; i--){
-            this.particles[i] = new Particle(robotState.x, robotState.y, robotState.dir, 1);
-        }
-    }else{
-    	// generate random particles within the map
-        var weight = 1.0/particleCount;
-        for (var i = particleCount - 1; i >= 0; i--){
-            this.particles[i] = this.newParticle();
-        }
-    }
+	if (typeof robotState !== 'undefined')
+	{
+		// If current robot state is already defined
+		// generate initial particles around the robot
+		for (var i = particleCount - 1; i >= 0; i--)
+		{
+			this.particles[i] = new Particle(robotState.x, robotState.y, robotState.dir, 1);
+		}
+	} else
+	{
+		// generate random particles within the map
+		var weight = 1.0 / particleCount;
+		for (var i = particleCount - 1; i >= 0; i--)
+		{
+			this.particles[i] = this.newParticle();
+		}
+	}
 }
 
 /**
@@ -36,45 +40,52 @@ function ParticleFilter(particleCount, motionModel, sensorModel, robotState){
  * @function
  * @param {Context} ctx - The context that the particles will be drawn on
  */
-ParticleFilter.prototype.draw = function(ctx){
-    for (var i = this.particles.length - 1; i >= 0; i--){
-        this.particles[i].draw(ctx);
-    }
+ParticleFilter.prototype.draw = function (ctx)
+{
+	for (var i = this.particles.length - 1; i >= 0; i--)
+	{
+		this.particles[i].draw(ctx);
+	}
 };
 
 /**
  * Regenerate all particles of the Particle filter
  * @function
  */
-ParticleFilter.prototype.regenrateAll = function() {
-    // generate random particles within the map
-    var weight = 1.0/this.count;
-    for (var i = this.count - 1; i >= 0; i--){
-        this.particles[i] = this.newParticle();
-    }
-}
+ParticleFilter.prototype.regenrateAll = function ()
+{
+	// generate random particles within the map
+	var weight = 1.0 / this.count;
+	for (var i = this.count - 1; i >= 0; i--)
+	{
+		this.particles[i] = this.newParticle();
+	}
+};
 
 /**
  * Refill all particles of the Particle filter
  * @function
  */
-ParticleFilter.prototype.refillAll = function() {
-    // generate random particles within the map
-    var weight = 1.0/this.count;
-    while (this.particles.length < this.count){
-        this.particles.push(this.newParticle());
-    }
-}
+ParticleFilter.prototype.refillAll = function ()
+{
+	// generate random particles within the map
+	var weight = 1.0 / this.count;
+	while (this.particles.length < this.count)
+	{
+		this.particles.push(this.newParticle());
+	}
+};
 
 /**
  * Return a new random particle
  * @function
  * @return {Particle} newParticle - A random generated particle
  */
-ParticleFilter.prototype.newParticle = function() {
-    return new Particle(this.sensorModel.width*random(), 
-            this.sensorModel.height*random(), TWO_PI*random(), 1.0);
-}
+ParticleFilter.prototype.newParticle = function ()
+{
+	return new Particle(this.sensorModel.width * random(),
+		this.sensorModel.height * random(), TWO_PI * random(), 1.0);
+};
 
 /**
  * Update the state of particles based on the odometry reading
@@ -82,12 +93,13 @@ ParticleFilter.prototype.newParticle = function() {
  * @param {Odometry} u - The odometry reading from the motion sensor
  * @param {float[]} z - An array of distance reading from the sensor
  */
-ParticleFilter.prototype.update = function(u, z){
-    if(typeof this.motionModel !== 'undefined')
-        this.motionUpdate(u);
+ParticleFilter.prototype.update = function (u, z)
+{
+	if (typeof this.motionModel !== 'undefined')
+		this.motionUpdate(u);
 
-    if(typeof this.sensorModel !== 'undefined')
-        this.sensorUpdate(z);
+	if (typeof this.sensorModel !== 'undefined')
+		this.sensorUpdate(z);
 };
 
 /**
@@ -95,15 +107,17 @@ ParticleFilter.prototype.update = function(u, z){
  * @function
  * @param {Odometry} u - The odometry reading from the motion sensor
  */
-ParticleFilter.prototype.motionUpdate = function(u){
-    // Update the state of all particles base on the estimated motion
-    for (var i = this.particles.length - 1; i >= 0; i--){
-        var p = this.particles[i];
-        var state = new RobotState(p.x, p.y, p.dir);
-        // Draw sample from p(x_t, u, x_t-1)
-        var newState = this.motionModel.sample(u, state);
-        p.setState(newState);
-    }
+ParticleFilter.prototype.motionUpdate = function (u)
+{
+	// Update the state of all particles base on the estimated motion
+	for (var i = this.particles.length - 1; i >= 0; i--)
+	{
+		var p = this.particles[i];
+		var state = new RobotState(p.x, p.y, p.dir);
+		// Draw sample from p(x_t, u, x_t-1)
+		var newState = this.motionModel.sample(u, state);
+		p.setState(newState);
+	}
 };
 
 /**
@@ -111,19 +125,21 @@ ParticleFilter.prototype.motionUpdate = function(u){
  * @function
  * @param {float[]} z - An array of distance reading from the sensor
  */
-ParticleFilter.prototype.sensorUpdate = function(z){
-    // Calculate the logs of weights
-    for(var i = this.particles.length-1; i >= 0; i--){
-        var p = this.particles[i];
-        p.w = this.sensorModel.probability(z, new RobotState(p.x, p.y, p.dir));
-    }
+ParticleFilter.prototype.sensorUpdate = function (z)
+{
+	// Calculate the logs of weights
+	for (var i = this.particles.length - 1; i >= 0; i--)
+	{
+		var p = this.particles[i];
+		p.w = this.sensorModel.probability(z, new RobotState(p.x, p.y, p.dir));
+	}
 
-    // To ensure the probability is in the range of (0,1)
-    // this.normalizeWeights();
+	// To ensure the probability is in the range of (0,1)
+	// this.normalizeWeights();
 
-    // Resample the particles
-    this.normalizeWeights();
-    this.resample(0.9);
+	// Resample the particles
+	this.normalizeWeights();
+	this.resample(0.9);
 };
 
 /**
@@ -131,22 +147,22 @@ ParticleFilter.prototype.sensorUpdate = function(z){
  * @function
  * @param {float} percent - The ratio of which will be resampled
  */
-ParticleFilter.prototype.resample = function(percent)
+ParticleFilter.prototype.resample = function (percent)
 {
 	var z_t = new Array(this.particles.length);
 
 	//Resample 80% of all particles, the rest 20% will be randomly generated
 	const m = z_t.length * percent;
 
-	const step = 1.0/m;
+	const step = 1.0 / m;
 
 	var cur = random() * step;
 
 	//Running sum
 	var cumulativeProbability = this.particles[0].w;
-	for(var i = 0, j = 0; i < m; i ++)
+	for (var i = 0, j = 0; i < m; i++)
 	{
-		while(j < this.particles.length-1 && cumulativeProbability < cur)
+		while (j < this.particles.length - 1 && cumulativeProbability < cur)
 		{
 			j++;
 			cumulativeProbability += this.particles[j].w;
@@ -155,9 +171,9 @@ ParticleFilter.prototype.resample = function(percent)
 		cur += step;
 	}
 
-	for(var i = m; i < z_t.length; i ++)
+	for (var i = m; i < z_t.length; i++)
 	{
-		z_t[i] = new Particle(random()*this.sensorModel.width, random()*this.sensorModel.height, random()*TWO_PI, 0);
+		z_t[i] = new Particle(random() * this.sensorModel.width, random() * this.sensorModel.height, random() * TWO_PI, 0);
 	}
 	this.particles = z_t;
 };
@@ -166,10 +182,11 @@ ParticleFilter.prototype.resample = function(percent)
  * Normalize the weight of the particles to range (1,0)
  * @function
  */
-ParticleFilter.prototype.normalizeWeights = function (){
+ParticleFilter.prototype.normalizeWeights = function ()
+{
 	var max = 0;
 	// Convert logs back to weights
-	for(var i = this.particles.length-1; i >= 0; i--)
+	for (var i = this.particles.length - 1; i >= 0; i--)
 	{
 		var p = this.particles[i];
 		// Normalize the logs
@@ -179,17 +196,20 @@ ParticleFilter.prototype.normalizeWeights = function (){
 		// p.w = Math.exp(p.w);
 		// sum += p.w;
 
-        if (max<p.w) max = p.w;
+		if (max < p.w) max = p.w;
 	}
 
-	if(max === 0)
+	if (max === 0)
 	{
-	    // If every particle has a probability of 0,
-        // just regenrate all particles
-        this.regenrateAll();
-	}else
+		// If every particle has a probability of 0,
+		// just regenrate all particles
+		this.regenrateAll();
+	} else
 	{
-	    // Otherwise normalize the weights
-		this.particles.forEach(function(p){p.w /= max});
+		// Otherwise normalize the weights
+		this.particles.forEach(function (p)
+		{
+			p.w /= max
+		});
 	}
 };
