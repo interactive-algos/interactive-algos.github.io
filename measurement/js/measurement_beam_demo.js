f376e3120303f948b0f98bfb5032c038a0088ec3
Kelvin Zhang <zhangxp1998@gmail.com>
2017-06-26 09:10:33 -0400

    shift to a more OO implementation

--- measurement/js/measurement_beam_demo.js
+++ measurement/js/measurement_beam_demo.js
@@ -42,7 +42,7 @@ BeamModelDemo.prototype.mouseDown = function (event)
 	const y = view.toWorldY(coor.y);
 	if (event.altKey)
 	{
-		if(this.shouldColor)
+		if (this.shouldColor)
 			return;
 		const w = this.sensorModel.probability(this.z, new RobotState(x, y, this.dir));
 		this.tracker.addParticle(new Particle(x, y, this.dir, w));
@@ -86,7 +86,9 @@ BeamModelDemo.prototype.mouseUp = function (event)
 	this.view.canvas.onmousemove = undefined;
 	this.view.canvas.onmouseout = undefined;
 	this.view.canvas.onmouseup = undefined;
+	this.tracker.clear();
 	this.update();
+	this.draw();
 	this.drawLaserLines();
 };
 
@@ -96,15 +98,20 @@ BeamModelDemo.prototype.draw = function ()
 	const ctx = this.view.ctx;
 	ctx.drawMap(this.map);
 	ctx.drawRobot(this.x, this.y, this.dir, this.robotSize);
-	if(!this.shouldColor)
+	if (!this.shouldColor)
 	{
 		const resolution = this.resolution;
-		this.tracker.forEach(function(x, y, dir, w)
+		this.tracker.forEach(function (x, y, dir, w)
 		{
-			console.log(w);
 			ctx.fillStyle = 'rgba(' + round(w * 255) + ', 0, ' + (255 - round(w * 255)) + ', 0.5)';
-			ctx.fillRect(x-resolution, y-resolution, resolution, resolution);
+			ctx.fillRect(x - resolution, y - resolution, resolution, resolution);
 		});
+	} else
+	{
+		//If user is moving its mouse, don't color the map, too expensive
+		if (this.view.canvas.onmousemove)
+			return;
+		this.view.colorMap(getColoringResolution(), this.sensorModel, this.z, this.dir);
 	}
 };
 
@@ -112,13 +119,13 @@ BeamModelDemo.prototype.update = function ()
 {
 	//Only scan if location changed
 	scan(this.x, this.y, this.dir, this.sensorRadius, this.map, this.z);
-	for(var i = 0; i < this.z.length; i ++)
+	for (var i = 0; i < this.z.length; i++)
 	{
-		this.z[i] += gaussian()*this.sensorModel.a1;
+		this.z[i] += gaussian() * this.sensorModel.a1;
 	}
 };
 
-BeamModelDemo.prototype.setNLasers = function(n)
+BeamModelDemo.prototype.setNLasers = function (n)
 {
 	this.z = new Array(n);
 	this.update();
@@ -126,7 +133,7 @@ BeamModelDemo.prototype.setNLasers = function(n)
 	this.drawLaserLines();
 };
 
-BeamModelDemo.prototype.setSensorRadius = function(r)
+BeamModelDemo.prototype.setSensorRadius = function (r)
 {
 	this.sensorRadius = r;
 	this.update();
@@ -134,7 +141,7 @@ BeamModelDemo.prototype.setSensorRadius = function(r)
 	this.drawLaserLines();
 };
 
-BeamModelDemo.prototype.setSensorNoise = function(p)
+BeamModelDemo.prototype.setSensorNoise = function (p)
 {
 	this.sensorModel.a1 = p;
 	this.update();
@@ -142,9 +149,16 @@ BeamModelDemo.prototype.setSensorNoise = function(p)
 	this.drawLaserLines();
 };
 
-BeamModelDemo.prototype.drawLaserLines = function()
+BeamModelDemo.prototype.drawLaserLines = function ()
 {
 	this.view.ctx.drawLaserLines(this.z, this.x, this.y, this.dir, this.sensorRadius);
+};
+
+BeamModelDemo.prototype.setColoring = function (shouldColor)
+{
+	this.shouldColor = shouldColor;
+	this.draw();
+	this.drawLaserLines();
 };
 
 function ParticleTracker()
@@ -184,194 +198,3 @@ ParticleTracker.prototype.forEach = function (callback)
 		}
 	}
 };
-
-var canvas;
-
-var robotX;
-var robotY;
-
-var robotSize = 0.2;
-var senseRadius = 3;
-
-var nLasers = 19;
-
-var robotDir = 0;
-var dirOffset = 0;
-
-var sensorNoise = 0.0;
-
-//Results of laser scan
-var z = new Array(nLasers);
-
-var sensorModel;
-
-var map;
-
-var clickedParticles = [];
-var maxW = Number.NEGATIVE_INFINITY;
-var minW = Number.POSITIVE_INFINITY;
-
-var shouldColor;
-
-var view;
-
-function update()
-{
-	//Only scan if location changed
-	console.log(senseRadius);
-	scan(robotX, robotY, robotDir, senseRadius, map, z);
-	shouldColor = document.getElementById('shouldColor');
-	if (shouldColor.checked)
-	{
-		clearCanvas(canvas);
-		canvas.getContext('2d').drawMap(map);
-		colorMap();
-	}
-	clickedParticles = [];
-}
-
-function drawParticles(ctx, particles)
-{
-	const resolution = getColoringResolution();
-	if (particles.length === 1)
-	{
-		var p = particles[0];
-		var w = p.w;
-		ctx.fillStyle = 'rgba(' + round(w * 255) + ', 0, ' + (255 - round(w * 255)) + ', 0.5)';
-		ctx.fillRect(p.x - resolution / 2, p.y - resolution / 2, resolution, resolution);
-	} else
-	{
-		particles.forEach(function (p)
-		{
-			var w = (p.w - minW) / (maxW - minW);
-			ctx.fillStyle = 'rgba(' + round(w * 255) + ', 0, ' + (255 - round(w * 255)) + ', 0.5)';
-			ctx.fillRect(p.x - resolution / 2, p.y - resolution / 2, resolution, resolution);
-		});
-	}
-}
-
-function parameterChanged(event)
-{
-	var target = event.target || event.srcElement;
-
-	if (target.id === 'fogOfWar')
-	{
-		senseRadius = Number(target.value);
-	}
-	else if (target.id === 'sensorNoise')
-	{
-		sensorNoise = Number(target.value);
-		sensorModel.a1 = sensorNoise;
-	}
-	else if (target.id === 'nLasers')
-	{
-		nLasers = Number(target.value) + 1;
-		z = new Array(nLasers);
-	}
-}
-
-function trackRobotDir(event)
-{
-	var coor = getClickLoc(event);
-	var x = view.toWorldX(coor.x);
-	var y = view.toWorldY(coor.y);
-
-	robotDir = atan2(y - robotY, x - robotX);
-	dirOffset = round(robotDir / Math.PI / 2 * (nLasers - 1) * 2);
-	clearCanvas(canvas);
-	canvas.getContext('2d').drawMap(map);
-	canvas.getContext('2d').drawRobot(robotX, robotY, robotDir, robotSize);
-}
-
-function mouseDown(event)
-{
-	var coor = getClickLoc(event);
-	var x = view.toWorldX(coor.x);
-	var y = view.toWorldY(coor.y);
-
-	//Do nothing if it is not a left button event
-	if (event.altKey && !shouldColor.checked)
-	{
-		var probability = sensorModel.probability(z, new RobotState(x, y, robotDir));
-		document.getElementById('probability').innerHTML = probability;
-		clickedParticles.push(new Particle(x, y, robotDir, probability));
-		maxW = max(maxW, probability);
-		minW = min(minW, probability);
-		clearCanvas(canvas);
-		var ctx = canvas.getContext('2d');
-		ctx.drawMap(map);
-		ctx.drawRobot(robotX, robotY, robotDir, robotSize);
-		ctx.drawLaserLines(z, robotX, robotY, robotDir, senseRadius);
-		drawParticles(ctx, clickedParticles);
-		return;
-	}
-
-	robotX = x;
-	robotY = y;
-	clearCanvas(canvas);
-	canvas.getContext('2d').drawMap(map);
-	canvas.getContext('2d').drawRobot(robotX, robotY, robotDir, robotSize);
-	canvas.onmousemove = trackRobotDir;
-	canvas.onmouseout = mouseUp;
-	canvas.onmouseup = mouseUp;
-}
-
-function mouseUp()
-{
-	canvas.onmousemove = undefined;
-	canvas.onmouseout = undefined;
-	canvas.onmouseup = undefined;
-	update();
-	view.canvas.getContext('2d').drawLaserLines(z, robotX, robotY, robotDir, senseRadius);
-}
-
-function init()
-{
-	canvas = document.getElementById('canvas');
-	view = new View(canvas, 1);
-
-	//Draw the map
-	map = getMap();
-	view.setPreviewScale(map);
-	canvas.getContext('2d').drawMap(map);
-	robotX = floor(random() * canvas.width);
-	robotY = floor(random() * canvas.height);
-	robotDir = random() * TWO_PI;
-	dirOffset = round(robotDir / Math.PI / 2 * (nLasers - 1) * 2);
-
-	update();
-
-	sensorModel = new BeamModel(getSensorNoise(), getSensorRadius(), map);
-
-	Particle.size = 0.2;
-
-	//Listen to mouse click events
-	canvas.onmousedown = mouseDown;
-	senseRadius = getSensorRadius();
-	sensorNoise = getSensorNoise();
-}
-
-function toggleColoring(event)
-{
-	var target = event.target || event.srcElement;
-	if (target.checked)
-	{
-		colorMap();
-	} else
-	{
-		clearColor();
-	}
-}
-
-function colorMap()
-{
-	view.colorMap(getColoringResolution(), sensorModel, z, robotDir);
-}
-
-function clearColor()
-{
-	clearCanvas(view.canvas);
-	view.canvas.getContext('2d').drawMap(map);
-	view.canvas.getContext('2d').drawRobot(robotX, robotY, robotDir, robotSize);
-	view.canvas.getContext('2d').drawLaserLines(z, robotX, robotY, robotDir, senseRadius);
-}
