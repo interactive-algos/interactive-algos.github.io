ff665f76f7d0eee52f2768d40272f93af343b0d8
Kelvin Zhang <zhangxp1998@gmail.com>
2017-06-26 21:46:30 -0400

    implement sensor demo

--- uncertainty/sensor_demo.js
+++ uncertainty/sensor_demo.js
@@ -2,31 +2,60 @@
  * Created by kelvinzhang on 6/26/17.
  */
 
-function SensorDemo(id, sensorRadius, sensorNoise)
+function SensorDemo(id, sensorNoise)
 {
 	const scale = 50;
 	this.view = new View(document.getElementById(id), scale);
 	this.ctx = this.view.canvas.getContext('2d');
 	this.robotSize = 0.2;
+	this.nBuckets = 100;
+
+	//create an 0 filled array of size this.nBuckets
+	this.buckets = Array.apply(null, new Array(this.nBuckets)).map(Number.prototype.valueOf,0);
+	this.maxCount = 0;
 
 	//Demo parameters
-	this.sensorRadius = sensorRadius;
+	// this.sensorRadius = sensorRadius;
 	this.sensorNoise = sensorNoise;
 
-	//Robot pose is fixed.
-	this.x = this.view.width * 0.2;
-	this.y = this.view.height/2;
-	this.dir = 0;
-
-	//Map is just a single line
-	this.map = [new Line(this.view.width*0.7, this.view.height * 0.9,
-		this.view.width*0.7, this.view.height*0.1)];
+	this.actualDistance = this.view.width/2;
 
 	this.draw();
 }
 
+SensorDemo.prototype.sample = function(n)
+{
+	this.ctx.strokeStyle = 'green';
+	for(var i = 0; i < n; i ++)
+	{
+		var reading = this.actualDistance + gaussian()*this.sensorNoise;
+		// this.draw();
+		// this.ctx.strokeLine(reading, 0, reading, this.view.height);
+		// if(reading > this.sensorRadius)reading = this.sensorRadius;
+		var index = round(reading/this.view.width * this.nBuckets);
+		this.buckets[index]++;
+		this.maxCount = max(this.maxCount, this.buckets[index]);
+	}
+	this.draw();
+};
+
 SensorDemo.prototype.draw = function()
 {
-	this.ctx.drawMap(this.map);
-	this.ctx.drawRobot(this.x, this.y, this.dir, this.robotSize);
+	const ctx = this.ctx;
+
+	clearCanvas(this.view.canvas);
+
+	//Draw the actual distance
+	ctx.strokeStyle = 'red';
+	ctx.strokeLine(this.actualDistance, 0, this.actualDistance, this.view.height);
+
+	//Draw sensor readings
+	ctx.strokeStyle = 'grey';
+	var x = 0;
+	const step =  1.0/this.buckets.length * this.view.width;
+	for(var i = 0; i < this.buckets.length; i ++, x+=step)
+	{
+		var count = this.buckets[i];
+		ctx.strokeLine(x+step/2, 0, x+step/2, count*1.0/this.maxCount*this.view.height);
+	}
 };
